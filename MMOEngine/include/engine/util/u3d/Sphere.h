fbdc5aa7ed7942635fa7cad9f43bc971a927bd34
lasko2112 <coll56667@yahoo.co.uk>
2015-11-30 13:52:54 +0000

    [Reset] Head to 55f63d9d42

---
+++ MMOEngine/include/engine/util/u3d/Sphere.h
@@ -0,0 +1,56 @@
+/*
+ * Sphere.h
+ *
+ *  Created on: 05/12/2010
+ *      Author: victor
+ */
+
+#ifndef SPHERE_H_
+#define SPHERE_H_
+
+#include "Vector3.h"
+#include "Triangle.h"
+
+namespace engine {
+ namespace util {
+   	namespace u3d {
+
+	class AABB;
+
+	class Sphere : public Object {
+		Vector3 origin;
+		float radius;
+		float radiusSquared;
+
+	public:
+		Sphere(const Vector3& orig, float rad) : origin(orig), radius(rad),
+			radiusSquared(rad * rad) {
+		}
+
+		bool intersects(const Triangle& tri) const;/* {
+			return (tri.distSqrd(origin) <= radiusSquared);
+		}*/
+		// does this intersect an aabb?
+		bool intersects(const AABB& aabb) const;
+		// renders a sphere
+
+		inline float getRadius() {
+			return radius;
+		}
+
+		inline float getRadiusSquared() {
+			return radiusSquared;
+		}
+
+		inline Vector3 getCenter() {
+			return origin;
+		}
+	};
+
+   	} // u3d
+  } // util
+} // engine
+
+using namespace engine::util::u3d;
+
+#endif /* SPHERE_H_ */
